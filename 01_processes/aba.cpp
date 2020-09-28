503b7aab8163e293fadaf3a980c0c220f60c4f4e
Klinger <klinger.jonathan@student.htlwrn.ac.at>
2020-09-28 20:12:50 +0200

    Signal hinzugefuegt um Kindprozess zu beenden.

--- 01_processes/aba.cpp
+++ 01_processes/aba.cpp
@@ -2,16 +2,24 @@
 #include <unistd.h>
 #include <chrono>
 #include <thread>
+#include <csignal>
 
 using namespace std;
 
 int main() {
+    int countB = 0;
     auto pid{fork()};
     if(pid == 0) {
         while(true) {
             cout << "B" << flush;
             chrono::milliseconds sleeptime(500);
             this_thread::sleep_for(sleeptime);
+            countB++;
+
+            if(countB == 6){
+                kill(pid, SIGKILL);
+                kill(getpid(), SIGTERM);
+            }
         } 
     } else {
        while(true) {
