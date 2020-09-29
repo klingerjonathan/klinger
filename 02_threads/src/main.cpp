098d24f85f75999fdcc43028f0373aba8ea7c773
Klinger <klinger.jonathan@student.htlwrn.ac.at>
2020-09-29 10:25:52 +0200

    erstelle klasse car. operator() nich nicht implementiert

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -3,19 +3,28 @@
 
 using namespace std;
 
-void mini_one() {
-    int round = 1;
+class Car {
+    string carName;
 
-    while(true){
-        this_thread::sleep_for(chrono::seconds(1));
-        cout << "Runde: " << round << endl << flush; 
-        round++;
+    Car(string name) {
+        carName = name;
     }
-}
+
+    void race() {
+        int round = 1;
+
+        while(true){
+            this_thread::sleep_for(chrono::seconds(1));
+            cout << "Runde: " << round << endl << flush; 
+            round++;
+        }
+    }
+};
+
+
 
 int main() {
-    thread t{mini_one};
-    t.join();
+
 }
 
 
