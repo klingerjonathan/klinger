975d7c468bb1af89002c6c43947ee024aac8c595
Klinger <klinger.jonathan@student.htlwrn.ac.at>
2020-10-14 09:26:25 +0200

    erledige 2.; Klasse Car fertiggestellt und threads in main starten.

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -4,26 +4,39 @@
 using namespace std;
 
 class Car {
-    string carBrand;
+    public:
+        void operator()() {
+            int round = 1;
 
-    void Car::operator()(string brand) {
-        carBrand = brand;
-    }
-
-    void race() {
-        int round = 1;
-
-        while(true){
-            this_thread::sleep_for(chrono::seconds(1));
-            cout << "Runde: " << round << endl << flush; 
-            round++;
+            while(true){
+                this_thread::sleep_for(chrono::seconds(1));
+                cout << "Car: Runde " << round << flush << endl; 
+                round++;
+            }
         }
-    }
+
 };
+
+void mini_one() {
+    int round = 1;
+
+    while(true){
+        this_thread::sleep_for(chrono::seconds(1));
+        cout << "Mini One: Runde " << round << flush << endl; 
+        round++;
+    }
+}
 
 
 
 int main() {
+    Car audi_a1;
+    
+    thread t1{audi_a1};
+    thread t2{mini_one};
+
+    t1.join();
+    t2.join();
 
 }
 
