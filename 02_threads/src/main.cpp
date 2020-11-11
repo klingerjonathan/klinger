d1d566c6487e726402685cf80f6f4af57f8333af
Klinger <klinger.jonathan@student.htlwrn.ac.at>
2020-11-11 08:17:43 +0100

    Füge Rundenzeit hinzu

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -1,47 +1,54 @@
 #include <iostream>
 #include <thread>
+#include <random>
 
 using namespace std;
 
-class Car {
-    public:
-        void operator()() {
-            int round = 1;
-            string output;
+class Car
+{
+public:
+    void operator()()
+    {
+        int round = 1;
+        string output;
 
-            while(true){
-                this_thread::sleep_for(chrono::seconds(1));
-                output = "Car: Runde " + to_string(round) + "\n"; 
-                cout << output << flush;
-                round++;
-            }
+        while (true)
+        {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_real_distribution<> dis{1, 10};
+            this_thread::sleep_for(chrono::milliseconds(1000));
+            output = "Car: Runde " + to_string(round) + " Rundenzeit: " + to_string(dis(gen)) + "\n";
+            cout << output << flush;
+            round++;
         }
-
+    }
 };
 
-void mini_one() {
+void mini_one()
+{
     int round = 1;
-    string output; 
+    string output;
 
-    while(true){
-        this_thread::sleep_for(chrono::seconds(1));
-        output = "Mini One: Runde " + to_string(round) + "\n"; 
+    while (true)
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dis{1, 10};
+        this_thread::sleep_for(chrono::milliseconds(1000));
+        output = "Mini One: Runde " + to_string(round) + " Rundenzeit: " + to_string(dis(gen)) + "\n";
         cout << output << flush;
         round++;
     }
 }
 
-
-
-int main() {
+int main()
+{
     Car audi_a1;
-    
+
     thread t1{audi_a1};
     thread t2{mini_one};
 
     t1.join();
     t2.join();
-
 }
-
-
