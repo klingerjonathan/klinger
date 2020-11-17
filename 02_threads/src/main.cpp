82ed42b9a2fa189ce76805a240a7ee113ca75497
Klinger <klinger.jonathan@student.htlwrn.ac.at>
2020-11-17 19:32:03 +0100

    Gesamtzeit wird berechnet, und Sieger und Gewinner ausgegeben (Punk 8.)

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -8,40 +8,51 @@ using namespace std;
 class Car
 {
 public:
+    float ges = 0;
+
     void operator()()
     {
         int round = 1;
         string output;
         ostringstream buf;
 
-        while (true)
+        for (int i = 0; i < 10; i++)
         {
             std::random_device rd;
             std::mt19937 gen(rd());
             std::uniform_real_distribution<> dis{1, 10};
             this_thread::sleep_for(chrono::milliseconds(1000));
-            buf << "Car: Runde " << round << " Rundenzeit: " << setprecision(3) << dis(gen) << endl;
+            float rundenzeit = dis(gen);
+            ges += rundenzeit;
+            buf << "Klasse Car: Runde " << round << " Rundenzeit: " << setprecision(3) << rundenzeit << "s" << endl;
             output = buf.str();
             buf.str("");
             cout << output << flush;
             round++;
         }
     }
+
+    int get_total_time()
+    {
+        return ges;
+    }
 };
 
-void mini_one()
+void mini_one(float &n)
 {
     int round = 1;
     string output;
     ostringstream buf;
 
-    while (true)
+    for (int i = 0; i < 10; i++)
     {
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_real_distribution<> dis{1, 10};
         this_thread::sleep_for(chrono::milliseconds(1000));
-        buf << "Mini One: Runde " << round << " Rundenzeit: " << setprecision(3) << dis(gen) << endl;
+        float rundenzeit = dis(gen);
+        n += rundenzeit;
+        buf << "Mini One: Runde " << round << " Rundenzeit: " << setprecision(3) << rundenzeit << "s" << endl;
         output = buf.str();
         buf.str("");
         cout << output << flush;
@@ -52,10 +63,22 @@ void mini_one()
 int main()
 {
     Car audi_a1;
+    float n{};
 
     thread t1{audi_a1};
-    thread t2{mini_one};
+    thread t2{mini_one, ref(n)};
 
     t1.join();
     t2.join();
+
+    if (n > audi_a1.get_total_time())
+    {
+        cout << "Der Sieger ist Mini mit   : " << n << endl;
+        cout << "Der Verlierer ist Audi mit: " << audi_a1.get_total_time() << endl;
+    } else if (n < audi_a1.get_total_time()) {
+        cout << "Der Sieger ist Audi mit      : " << audi_a1.get_total_time() << endl;
+        cout << "Der Verlierer ist Mini mit   : " << n << endl;
+    } else {
+        cout << "Unentschieden mit: " << n << endl; 
+    }
 }
