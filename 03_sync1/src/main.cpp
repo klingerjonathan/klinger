ebe73a0c7407627261c771286cdf38fd617a4062
Klinger <klinger.jonathan@student.htlwrn.ac.at>
2020-12-27 06:10:44 +0100

    Ordner für Exercise 3 Angelegt und mit 03 begonnen

---
+++ 03_sync1/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+auth: Jonathan Klinger
+file: main.cpp
+date: 12.20
+desc: Exercise 03 NVS
+*/
+
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+    cout << "Hello World!" << endl;
+}
