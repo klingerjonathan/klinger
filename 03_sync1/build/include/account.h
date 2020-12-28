10fc3443e7fe42519d180494bedaa983aaae8030
Jonathan <klinger603@gmail.com>
2020-12-28 20:09:24 +0100

    erstelle account.h inklusive Klasse Account und geforderte Member Functions

---
+++ 03_sync1/build/include/account.h
@@ -0,0 +1,16 @@
+/*
+auth: Jonathan Klinger
+file: account.h
+date: 12.20
+desc: Exercise 03 NVS
+*/
+
+class Account {
+    private: 
+        int balance = 1; 
+
+    public: 
+        int get_balance();
+        bool deposit(int amount) ;
+        bool withdraw(int amount);
+};
