adffda15b4832c676d6dfdf1c581dafd50e35c04
Jonathan <klinger603@gmail.com>
2020-12-28 20:41:42 +0100

    bessere thread fehler aus

--- 03_sync1/src/main.cpp
+++ 03_sync1/src/main.cpp
@@ -7,11 +7,14 @@ desc: Exercise 03 NVS
 
 #include <iostream>
 #include "account.h"
+#include <thread>
 
 using namespace std; 
 
 int main() {
     Account klinger;
+
+    /* P U N K T 1
     klinger.deposit(15);
     klinger.withdraw(6);
     //zu Viel abheben
@@ -19,5 +22,23 @@ int main() {
 
     cout << "Status: " << erfolg << endl;
     cout << "Balance: " << klinger.get_balance() << endl;
+    */ 
+
+    klinger.deposit(1);
+
+    thread t1{[&]() {
+        klinger.withdraw(1);
+    }};
+
+    thread t2{[&]() {
+        klinger.withdraw(1);
+    }};
+
+    t1.join();
+    t2.join();
+
+    cout << "Balance: " << klinger.get_balance() << endl;
+
+    return 0;
 }
 
