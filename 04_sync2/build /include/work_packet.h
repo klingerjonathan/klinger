da8ae5011ac3da708f146af33f8ab54ce35bd00f
Jonathan <klinger603@gmail.com>
2020-12-29 23:44:59 +0100

    starte 04_sync2

---
+++ 04_sync2/build /include/work_packet.h
@@ -0,0 +1,6 @@
+/*
+auth: Jonathan Klinger 
+file: work_packet.h 
+date: 12.2020
+desc: Exercise 04
+*/
---
+++ 04_sync2/src/loadsim.cpp
@@ -0,0 +1,10 @@
+/*
+auth: Jonathan Klinger 
+file: loadsim.cpp 
+date: 12.2020
+desc: Exercise 04
+*/
+
+int main() {
+    
+}
