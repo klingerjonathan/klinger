b6f99f5491c4d8eadf7739954f5a8157ae270907
Jonathan <klinger603@gmail.com>
2020-12-30 00:11:23 +0100

    füge WorkQueue und erstes loadsim Program hinzu

--- 04_sync2/src/loadsim.cpp
+++ 04_sync2/src/loadsim.cpp
@@ -6,9 +6,21 @@ desc: Exercise 04
 */
 
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include "work_queue.h"
+#include "work_packet.h"
 
 using namespace std;
 
 int main() {
-    cout << "hello World" << endl;
+    int counter{0};
+
+    WorkQueue wq;
+    while (true){
+        wq.push(WorkPacket{counter});
+        cout << "B: Submitted work packet " << counter << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+        counter++;
+    }
 }
--- 04_sync2/src/work_queue.cpp
+++ 04_sync2/src/work_queue.cpp
@@ -5,3 +5,27 @@ date: 12.2020
 desc: Exercise 04
 */
 
+#include <thread>
+#include <mutex>
+#include <queue>
+#include "work_queue.h"
+#include "work_packet.h"
+
+using namespace std;
+
+WorkQueue::WorkQueue(){ }
+
+void WorkQueue::push(WorkPacket wp){
+    lock_guard<mutex> guard{m};
+    this->work_queue.push(wp);
+}
+
+bool WorkQueue::pop(){
+    lock_guard<mutex> guard{m};
+    if(!this->work_queue.empty()){
+        this->work_queue.pop();
+        return true;
+    } else {
+        return false;
+    }
+}
