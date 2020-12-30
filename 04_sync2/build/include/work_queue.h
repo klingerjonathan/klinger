63e6800fa73572e992f23084a6b9ee406efdc227
Jonathan <klinger603@gmail.com>
2020-12-30 03:46:25 +0100

    füge boss hinzu bis ca. 6 fertig

--- 04_sync2/build/include/work_queue.h
+++ 04_sync2/build/include/work_queue.h
@@ -20,9 +20,11 @@ class WorkQueue{
         queue<WorkPacket> work_queue;
         mutex m;
         std::condition_variable not_empty;
-        
+        std::condition_variable is_full;
+        size_t max_packets;
+
     public:
-        WorkQueue();
+        WorkQueue(size_t max) {max_packets = max;};
         void push(WorkPacket wp);
         WorkPacket pop();
 };
