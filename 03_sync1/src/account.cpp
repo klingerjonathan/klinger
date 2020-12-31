4f3b659554c4d2b797bbda79791ddda9cf8e2c61
Jonathan <klinger603@gmail.com>
2021-01-01 00:11:47 +0100

    verbessere Formatierung; 03 beendet

--- 03_sync1/src/account.cpp
+++ 03_sync1/src/account.cpp
@@ -5,44 +5,40 @@ date: 12.20
 desc: Exercise 03 NVS
 */
 
-#include <thread>
-#include <mutex>
-#include <chrono>
 #include "account.h"
+#include <chrono>
+#include <mutex>
+#include <thread>
 
 using namespace std;
 
-
-int Account::get_balance() {
-    return balance; 
-}
+int Account::get_balance() { return balance; }
 
 bool Account::deposit(int amount) {
-    unique_lock<mutex> unique{m};
-    int tmp{this->balance};
-    this_thread::sleep_for(chrono::milliseconds{10});
-    this->balance += amount;
-    return 1;
-}
-        
-        
-bool Account::withdraw(int amount) {
-    lock_guard<mutex> guard{m};
-    if (balance - amount < 0) {
-        return false;
-    } else {
-        balance -= amount;
-        return true;
-    }
+  unique_lock<mutex> unique{m};
+  int tmp{this->balance};
+  this_thread::sleep_for(chrono::milliseconds{10});
+  this->balance += amount;
+  return 1;
 }
 
-Depositer::Depositer(Account* account, int init_deposits) {
-    this->account = account;
-    this->start_deposits = init_deposits;
+bool Account::withdraw(int amount) {
+  lock_guard<mutex> guard{m};
+  if (balance - amount < 0) {
+    return false;
+  } else {
+    balance -= amount;
+    return true;
+  }
+}
+
+Depositer::Depositer(Account *account, int init_deposits) {
+  this->account = account;
+  this->start_deposits = init_deposits;
 }
 
 void Depositer::operator()() {
-    for(int i=0; i<this->start_deposits; i++) {
-        this->account->deposit(1);
-    }
+  for (int i = 0; i < this->start_deposits; i++) {
+    this->account->deposit(1);
+  }
 }
--- 03_sync1/src/main.cpp
+++ 03_sync1/src/main.cpp
@@ -5,76 +5,74 @@ date: 12.20
 desc: Exercise 03 NVS
 */
 
+#include "CLI11.hpp"
+#include "account.h"
 #include <iostream>
 #include <thread>
-#include "account.h"
-#include "CLI11.hpp"
 
-
-using namespace std; 
+using namespace std;
 
 int main(int argc, char **argv) {
-    Account klinger;
+  Account klinger;
 
-    /* P U N K T 1
-    klinger.deposit(15);
-    klinger.withdraw(6);
-    //zu Viel abheben
-    bool erfolg = klinger.withdraw(10);
+  /* P U N K T 1
+  klinger.deposit(15);
+  klinger.withdraw(6);
+  //zu Viel abheben
+  bool erfolg = klinger.withdraw(10);
 
-    cout << "Status: " << erfolg << endl;
-    cout << "Balance: " << klinger.get_balance() << endl;
-    */ 
+  cout << "Status: " << erfolg << endl;
+  cout << "Balance: " << klinger.get_balance() << endl;
+  */
 
-    /*
-    klinger.deposit(1);
+  /*
+  klinger.deposit(1);
 
-    thread t1{[&]() {
-        klinger.withdraw(1);
-    }};
+  thread t1{[&]() {
+      klinger.withdraw(1);
+  }};
 
-    thread t2{[&]() {
-        klinger.withdraw(1);
-    }};
+  thread t2{[&]() {
+      klinger.withdraw(1);
+  }};
 
-    t1.join();
-    t2.join();
+  t1.join();
+  t2.join();
 
-    cout << "Balance: " << klinger.get_balance() << endl;
-    */
+  cout << "Balance: " << klinger.get_balance() << endl;
+  */
 
-/*
-    Depositer dep1(&klinger);
-    Depositer dep2(&klinger);
+  /*
+      Depositer dep1(&klinger);
+      Depositer dep2(&klinger);
 
-    thread t1{&Depositer::operator(), ref(dep1)};
-    thread t2{&Depositer::operator(), ref(dep2)};
+      thread t1{&Depositer::operator(), ref(dep1)};
+      thread t2{&Depositer::operator(), ref(dep2)};
 
-    t1.join();
-    t2.join();
-    cout << "Balance: " << klinger.get_balance() << endl;
+      t1.join();
+      t2.join();
+      cout << "Balance: " << klinger.get_balance() << endl;
 
-*/
+  */
 
-    CLI::App app("Account app");
+  CLI::App app("Account app");
 
-    int balance{0};
-    app.add_option("balance", balance, "Initial balance")->required();
-    int deposits{5};
-    app.add_option("-d,--deposits", deposits, "Count of deposits", true);
-    CLI11_PARSE(app, argc, argv);
+  int balance{0};
+  app.add_option("balance", balance, "Initial balance")->required();
+  int deposits{5};
+  app.add_option("-d,--deposits", deposits, "Count of deposits", true);
+  CLI11_PARSE(app, argc, argv);
 
-    klinger.deposit(balance);
+  klinger.deposit(balance);
 
-    Depositer dep1(&klinger, deposits);
-    Depositer dep2(&klinger, deposits);
+  Depositer dep1(&klinger, deposits);
+  Depositer dep2(&klinger, deposits);
 
-    thread t1{&Depositer::operator(), ref(dep1)};
-    thread t2{&Depositer::operator(), ref(dep2)};
+  thread t1{&Depositer::operator(), ref(dep1)};
+  thread t2{&Depositer::operator(), ref(dep2)};
 
-    t1.join();
-    t2.join();
-    cout << klinger.get_balance() << endl;
-    return 0;
+  t1.join();
+  t2.join();
+  cout << klinger.get_balance() << endl;
+  return 0;
 }
-
