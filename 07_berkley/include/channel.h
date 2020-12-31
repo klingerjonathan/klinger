8a74735b542d11160d3b847655de00703e445c7a
Jonathan <klinger603@gmail.com>
2020-12-31 18:26:49 +0100

    implementiere channel.h; Punkt 8 fertig

---
+++ 07_berkley/include/channel.h
@@ -0,0 +1,33 @@
+/*
+ * auth: Jonathan Klinger
+ * file: channel.h
+ * date: 12/20
+ * desc: Exercise 10 - berkely
+ */
+
+
+#pragma once
+
+#include "pipe.h"
+
+class Channel
+{
+private:
+  Pipe<long> pipe1;
+  Pipe<long> pipe2;
+
+public:
+  Channel() {}
+
+  Pipe<long> &get_pipe1()
+  {
+    return pipe1;
+  }
+
+  Pipe<long> &get_pipe2()
+  {
+    return pipe2;
+  }
+
+
+};
