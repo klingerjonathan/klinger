90bceb64aabf1471d6a0c006067652e85659cf8c
Jonathan <klinger603@gmail.com>
2021-01-01 00:25:08 +0100

    verbessere Formatierung; 06 fertig

--- 06_future/include/calc_factors.h
+++ 06_future/include/calc_factors.h
@@ -8,27 +8,28 @@
 
 #include "InfInt.h"
 
-inline std::vector<InfInt> get_factors(InfInt number) { // number should be positive
-    std::vector<InfInt> factors;
-    while (number % 2 == InfInt::zero) {
-        number = number / 2;
-        factors.push_back(InfInt::two);
-    }
-    InfInt limit = number.intSqrt() + InfInt::one;
-    
- factor:
-    for (InfInt i=3; i <= limit; i = i + InfInt::two) {
-        while (number % i == 0) {
-            number = number / i;
-            factors.push_back(i);
-            if (number == InfInt::one)
-                goto factor;
-            limit = number.intSqrt() + InfInt::one;
-        }
-    }
+inline std::vector<InfInt>
+get_factors(InfInt number) { // number should be positive
+  std::vector<InfInt> factors;
+  while (number % 2 == InfInt::zero) {
+    number = number / 2;
+    factors.push_back(InfInt::two);
+  }
+  InfInt limit = number.intSqrt() + InfInt::one;
 
-    if (number != InfInt::zero || factors.size() == 0)
-        factors.push_back(number);
-    return factors;
+factor:
+  for (InfInt i = 3; i <= limit; i = i + InfInt::two) {
+    while (number % i == 0) {
+      number = number / i;
+      factors.push_back(i);
+      if (number == InfInt::one)
+        goto factor;
+      limit = number.intSqrt() + InfInt::one;
+    }
+  }
+
+  if (number != InfInt::zero || factors.size() == 0)
+    factors.push_back(number);
+  return factors;
 }
 #endif
--- 06_future/src/main.cpp
+++ 06_future/src/main.cpp
@@ -5,100 +5,95 @@
  * desc: Exercise 06 - future
  */
 
+#include "CLI11.hpp"
+#include "InfInt.h"
+#include "calc_factors.h"
+#include <chrono>
+#include <future>
 #include <iostream>
 #include <vector>
-#include <future>
-#include <chrono>
-#include "CLI11.hpp"
-#include "calc_factors.h"
-#include "InfInt.h"
 
 using namespace std;
 
-string validate(const string& numbers) {
-    size_t check{numbers.find_first_not_of("0123456789")};
-    if(check == string::npos){
-        return string();
-    }
-    else{
-        return string(numbers) + " contains not numeric character";
-    }  
+string validate(const string &numbers) {
+  size_t check{numbers.find_first_not_of("0123456789")};
+  if (check == string::npos) {
+    return string();
+  } else {
+    return string(numbers) + " contains not numeric character";
+  }
 }
 
-void factoring(vector<InfInt>& numbers, vector<shared_future<vector<InfInt>>>& futures) {
-    futures.at(numbers.size() - 1).wait();
+void factoring(vector<InfInt> &numbers,
+               vector<shared_future<vector<InfInt>>> &futures) {
+  futures.at(numbers.size() - 1).wait();
 
-    for (unsigned int i{}; i < numbers.size(); i++){
-        cout << numbers[i] << ": ";
-        futures[i].wait();
-        for (auto primes : futures[i].get())
-        {
-            cout << primes.toString() << " ";
-        }
-        cout << endl;
+  for (unsigned int i{}; i < numbers.size(); i++) {
+    cout << numbers[i] << ": ";
+    futures[i].wait();
+    for (auto primes : futures[i].get()) {
+      cout << primes.toString() << " ";
     }
-               }
+    cout << endl;
+  }
+}
 
-void checkFacts(vector<InfInt>& numbers, vector<shared_future<vector<InfInt>>>& futures) {
-    for (unsigned int i=0; i<futures.size(); i++) {
-        futures[i].wait();
-        InfInt res = 1;
-        for (auto elem : futures[i].get()) {
-            res *= elem;
-        }
-        if (res != numbers[i]) {
-            cerr << "Factoring FAILED for: " << numbers.at(i) << endl;
-        }
+void checkFacts(vector<InfInt> &numbers,
+                vector<shared_future<vector<InfInt>>> &futures) {
+  for (unsigned int i = 0; i < futures.size(); i++) {
+    futures[i].wait();
+    InfInt res = 1;
+    for (auto elem : futures[i].get()) {
+      res *= elem;
     }
+    if (res != numbers[i]) {
+      cerr << "Factoring FAILED for: " << numbers.at(i) << endl;
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
-    CLI::App app("Factor numbers");
+  CLI::App app("Factor numbers");
 
-    vector<string> numbers{};
-    app.add_option("number", numbers, "Zahlen zum Faktorieren")
-        ->required()
-        ->check(validate);
+  vector<string> numbers{};
+  app.add_option("number", numbers, "Zahlen zum Faktorieren")
+      ->required()
+      ->check(validate);
 
-    bool sync = false; 
-    app.add_flag("-a, --async", sync, "async");
+  bool sync = false;
+  app.add_flag("-a, --async", sync, "async");
 
-    CLI11_PARSE(app, argc, argv);
+  CLI11_PARSE(app, argc, argv);
 
-    vector<InfInt> facts{};
+  vector<InfInt> facts{};
 
+  for (unsigned int i = 0; i < numbers.size(); i++) {
+    facts.push_back(numbers[i]);
+  }
 
-    for (unsigned int i=0; i<numbers.size(); i++) {
-        facts.push_back(numbers[i]);
+  vector<shared_future<vector<InfInt>>> futures;
+
+  if (sync) {
+    for (auto elem : facts) {
+      futures.push_back(async(launch::async, get_factors, elem));
     }
-
-    vector<shared_future<vector<InfInt>>> futures;
-
-    if (sync) {
-        for (auto elem : facts) {
-            futures.push_back(async(launch::async, get_factors, elem));
-        }
-    } else {
-        for (auto elem : facts) {
-            futures.push_back(async(get_factors, elem));
-        }
+  } else {
+    for (auto elem : facts) {
+      futures.push_back(async(get_factors, elem));
     }
-    
-    
+  }
 
-    auto start = chrono::system_clock::now();
+  auto start = chrono::system_clock::now();
 
-    thread t1{factoring, ref(facts), ref(futures)};
-    thread t2{checkFacts, ref(facts), ref(futures)};
-    t1.join();
-    t2.join();
+  thread t1{factoring, ref(facts), ref(futures)};
+  thread t2{checkFacts, ref(facts), ref(futures)};
+  t1.join();
+  t2.join();
 
-    auto duration = chrono::duration_cast<chrono::milliseconds>
-    (std::chrono::system_clock::now() - start);
-    cout << "Time elapsed used for factoring: " << duration.count() << "ms" << endl;
-    
+  auto duration = chrono::duration_cast<chrono::milliseconds>(
+      std::chrono::system_clock::now() - start);
+  cout << "Time elapsed used for factoring: " << duration.count() << "ms"
+       << endl;
 
-    return 0;
-
-
+  return 0;
 }
