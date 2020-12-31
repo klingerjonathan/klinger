d40a0a8e92f86af489a83e69bdc1265036cb1118
Jonathan <klinger603@gmail.com>
2020-12-31 18:29:36 +0100

    implementiere get_channel(); Punkt 9 fertig

--- 07_berkley/include/TimeSlave.h
+++ 07_berkley/include/TimeSlave.h
@@ -12,16 +12,22 @@
 using namespace std;
 
 #include "clock.h"
+#include "channel.h"
 
 class TimeSlave {
 private:
   Clock clock;
   string name;
+  Channel* channel;
 
 public:
   TimeSlave(std::string name, int hours, int minutes, int seconds) : clock(name + "_name", hours, minutes, seconds) {
       this->name = name;
   };
+
+  Channel* get_channel() {
+      return channel;
+  }
   
   void operator()();
 };
