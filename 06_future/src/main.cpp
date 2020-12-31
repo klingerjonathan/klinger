249ec5570697bfc40bccd94247fc24af1188183a
Jonathan <klinger603@gmail.com>
2020-12-31 04:12:35 +0100

    staged commit übersehen :/

--- 06_future/src/main.cpp
+++ 06_future/src/main.cpp
@@ -5,6 +5,35 @@
  * desc: Exercise 06 - future
  */
 
-int main(int argc, char** argv) {
-    return 0;
+//#include "InfInt.h"
+#include <iostream>
+#include <vector>
+#include "CLI11.hpp"
+
+using namespace std;
+
+string validate(const string& numbers) {
+    size_t check{numbers.find_first_not_of("0123456789")};
+    if(check == string::npos){
+        return string();
+    }
+    else{
+        return string(numbers) + " contains not numeric character";
+    }  
+}
+
+int main(int argc, char *argv[]) {
+    CLI::App app("Factor numbers");
+
+    vector<string> numbers{};
+    app.add_option("number", numbers, "Zahlen zum Faktorieren")
+        ->required()
+        ->check(validate);
+
+    CLI11_PARSE(app, argc, argv);
+
+    for (int i=0; i<numbers.size(); i++) {
+        cout << numbers[i] << endl;
+    }
+
 }
