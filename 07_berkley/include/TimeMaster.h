feaef49707f906861c70c289db578c5034afc91a
Jonathan <klinger603@gmail.com>
2020-12-31 18:33:37 +0100

    implementiere Channels in TimeMaster; Punkt 10

--- 07_berkley/include/TimeMaster.h
+++ 07_berkley/include/TimeMaster.h
@@ -12,16 +12,27 @@
 using namespace std;
 
 #include "clock.h"
+#include "channel.h"
 
 class TimeMaster {
 private:
   Clock clock;
   string name;
+  Channel* channel1;
+  Channel* channel2;
 
 public:
   TimeMaster(string name, int hours, int minutes, int seconds) : clock(name + "_name", hours, minutes, seconds) {
       this->name = name;
   };
+
+  void set_channel1(Channel* chan) {
+      channel1 = chan;
+  }
+
+  void set_channel2(Channel* chan) {
+      channel2 = chan;
+  }
   
   void operator()();
 };
